7fd1519129fa644eccc4a98e2e356d593ffb64c8
Ian Lee <87459518+DonghyeopLee@users.noreply.github.com>
2024-09-20 17:59:17 +1000

    initialize

---
+++ rest-demo/src/test/java/com/Ian/rest_demo/RestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ian.rest_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
